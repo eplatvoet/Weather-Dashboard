7956bf6f3f2779c31f2465cb96a2bab606117c15
Erin Platvoet <erin.didomenico520@gmail.com>
2020-08-11 10:04:23 -0400

    working on local storage, got it to save

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
 
     //DATE AND TIME DISPLAYED
     $(".currentDay").text("Local Date & Time: " + moment().format('MMMM Do YYYY, h:mm a'));
-
+    
     //FUNCTION FOR UPDATING TIME WITHOUT REFRESHING THE PAGE
     function updateClock() {
         $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
     }
-
+    
     setInterval(updateClock, 1000);
-    console.log("the current time is " + moment());
+    
 
     //ONCE USER ENTERS CITY AND PRESSES BUTTON:
     $("#getWeatherForecast").click(function () {
@@ -26,8 +26,8 @@ $(document).ready(function () {
         .then(function (response) {
             console.log(queryURL);
             console.log(response);
-            // getWeatherForecast.push(city)
-            // window.localStorage.setItem("City", JSON.stringify(city))
+            storage.push(city);
+            window.localStorage.setItem("Cities: ", JSON.stringify(storage));
             
             //BEGIN DISPLAYING INFO FROM API
             $(".city").html("<h2>Currently in " + response.name + ":</h2><br>");
@@ -47,7 +47,7 @@ $(document).ready(function () {
             var lon = response.coord.lon
             var queryTwo = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + apiKey;
             
-            //RETRIEVING INFORMATION FROM OPENWEATHER API: ONE CALL API
+            //RETRIEVING MORE INFORMATION FROM OPENWEATHER API: ONE CALL API
             $.ajax({
                 url: queryTwo,
                 method: "GET"
@@ -57,60 +57,68 @@ $(document).ready(function () {
                 console.log(responseTwo)
                 $(".uvIndex").text("UV Index: " + responseTwo.current.uvi);
                 console.log(parseInt(responseTwo.current.uvi))
+                //CHANGING COLOR FOR THE UV INDEX
                 if(parseInt(responseTwo.current.uvi) >= 8){
-                        $(".uvIndex").addClass("severe");
-                    } else if(parseInt(responseTwo.current.uvi) < 8 && parseInt(responseTwo.current.uvi) > 4) {
-                            $(".uvIndex").addClass("moderate");
-                        } else {
-                                $(".uvIndex").addClass("favorable")
-                            };
-                            
-                            
-                            //* ADD DATE TO FORECAST *
-                            //FORECASTED DAY 1
-                            // $(".dateOne").text(moment().add(1, "d");
-                            $(".weatherOne").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[1].weather[0].icon + ".png'>");
-                            var highOne = (responseTwo.daily[1].temp.max - 273.15) * 1.80 + 32;
-                            var lowOne = (responseTwo.daily[1].temp.min - 273.15) * 1.80 + 32;
-                            $(".highOne").html("High: " + highOne.toFixed(2) + "&deg;F");
-                            $(".lowOne").html("Low: " + lowOne.toFixed(2) + "&deg;F");
-                            $(".humidityOne").text("Humidity: " + responseTwo.daily[1].humidity + "%");
-                            
-                            //FORECASTED DAY 2
-                            $(".weatherTwo").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[2].weather[0].icon + ".png'>");
-                            var highTwo = (responseTwo.daily[2].temp.max - 273.15) * 1.80 + 32;
-                            var lowTwo = (responseTwo.daily[2].temp.min - 273.15) * 1.80 + 32;
-                            $(".highTwo").html("High: " + highTwo.toFixed(2) + "&deg;F");
-                            $(".lowTwo").html("Low: " + lowTwo.toFixed(2) + "&deg;F");
-                            $(".humidityTwo").text("Humidity: " + responseTwo.daily[2].humidity + "%");
-                            
-                            //FORECASTED DAY 3
-                            $(".weatherThree").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[3].weather[0].icon + ".png'>");
-                            var highThree = (responseTwo.daily[3].temp.max - 273.15) * 1.80 + 32;
-                            var lowThree = (responseTwo.daily[3].temp.min - 273.15) * 1.80 + 32;
-                            $(".highThree").html("High: " + highThree.toFixed(2) + "&deg;F");
-                            $(".lowThree").html("Low: " + lowThree.toFixed(2) + "&deg;F");
-                            $(".humidityThree").text("Humidity: " + responseTwo.daily[3].humidity + "%");
-                            
-                            //FORECASTED DAY 4
-                            $(".weatherFour").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[4].weather[0].icon + ".png'>");
-                            var highFour = (responseTwo.daily[4].temp.max - 273.15) * 1.80 + 32;
-                            var lowFour = (responseTwo.daily[4].temp.min - 273.15) * 1.80 + 32;
-                            $(".highFour").html("High: " + highFour.toFixed(2) + "&deg;F");
-                            $(".lowFour").html("Low: " + lowFour.toFixed(2) + "&deg;F");
-                            $(".humidityFour").text("Humidity: " + responseTwo.daily[4].humidity + "%");
-                            
-                            //FORECASTED DAY 5
-                            $(".weatherFive").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[5].weather[0].icon + ".png'>");
-                            var highFive = (responseTwo.daily[5].temp.max - 273.15) * 1.80 + 32;
-                            var lowFive = (responseTwo.daily[5].temp.min - 273.15) * 1.80 + 32;
-                            $(".highFive").html("High: " + highFive.toFixed(2) + "&deg;F");
-                            $(".lowFive").html("Low: " + lowFive.toFixed(2) + "&deg;F");
-                            $(".humidityFive").text("Humidity: " + responseTwo.daily[5].humidity + "%");
-                            
-                        })
-                    })
-                })
-    // 
-    //  $(".searchHistory").val(localStorage.getItem("City:", city));
-            });
+                    $(".uvIndex").addClass("severe");
+                } else if(parseInt(responseTwo.current.uvi) < 8 && parseInt(responseTwo.current.uvi) > 4) {
+                    $(".uvIndex").addClass("moderate");
+                } else {
+                    $(".uvIndex").addClass("favorable")
+                };
+                
+                
+                //* ADD DATE TO FORECAST *
+                //FORECASTED DAY 1
+                // $(".dateOne").text(moment().add(1, "d");
+                // $(".dateOne").text(format("mm/dd/yy", (parseInt(responseTwo.daily[1].dt ,10))));
+                
+                $(".weatherOne").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[1].weather[0].icon + ".png'>");
+                var highOne = (responseTwo.daily[1].temp.max - 273.15) * 1.80 + 32;
+                var lowOne = (responseTwo.daily[1].temp.min - 273.15) * 1.80 + 32;
+                $(".highOne").html("High: " + highOne.toFixed(2) + "&deg;F");
+                $(".lowOne").html("Low: " + lowOne.toFixed(2) + "&deg;F");
+                $(".humidityOne").text("Humidity: " + responseTwo.daily[1].humidity + "%");
+                
+                //FORECASTED DAY 2
+                $(".weatherTwo").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[2].weather[0].icon + ".png'>");
+                var highTwo = (responseTwo.daily[2].temp.max - 273.15) * 1.80 + 32;
+                var lowTwo = (responseTwo.daily[2].temp.min - 273.15) * 1.80 + 32;
+                $(".highTwo").html("High: " + highTwo.toFixed(2) + "&deg;F");
+                $(".lowTwo").html("Low: " + lowTwo.toFixed(2) + "&deg;F");
+                $(".humidityTwo").text("Humidity: " + responseTwo.daily[2].humidity + "%");
+                
+                //FORECASTED DAY 3
+                $(".weatherThree").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[3].weather[0].icon + ".png'>");
+                var highThree = (responseTwo.daily[3].temp.max - 273.15) * 1.80 + 32;
+                var lowThree = (responseTwo.daily[3].temp.min - 273.15) * 1.80 + 32;
+                $(".highThree").html("High: " + highThree.toFixed(2) + "&deg;F");
+                $(".lowThree").html("Low: " + lowThree.toFixed(2) + "&deg;F");
+                $(".humidityThree").text("Humidity: " + responseTwo.daily[3].humidity + "%");
+                
+                //FORECASTED DAY 4
+                $(".weatherFour").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[4].weather[0].icon + ".png'>");
+                var highFour = (responseTwo.daily[4].temp.max - 273.15) * 1.80 + 32;
+                var lowFour = (responseTwo.daily[4].temp.min - 273.15) * 1.80 + 32;
+                $(".highFour").html("High: " + highFour.toFixed(2) + "&deg;F");
+                $(".lowFour").html("Low: " + lowFour.toFixed(2) + "&deg;F");
+                $(".humidityFour").text("Humidity: " + responseTwo.daily[4].humidity + "%");
+                
+                //FORECASTED DAY 5
+                $(".weatherFive").html("<img src='https://openweathermap.org/img/w/" + responseTwo.daily[5].weather[0].icon + ".png'>");
+                var highFive = (responseTwo.daily[5].temp.max - 273.15) * 1.80 + 32;
+                var lowFive = (responseTwo.daily[5].temp.min - 273.15) * 1.80 + 32;
+                $(".highFive").html("High: " + highFive.toFixed(2) + "&deg;F");
+                $(".lowFive").html("Low: " + lowFive.toFixed(2) + "&deg;F");
+                $(".humidityFive").text("Humidity: " + responseTwo.daily[5].humidity + "%");
+                
+            })
+        })
+    })
+    var storage = JSON.parse(window.localStorage.getItem("Cities: ")) || [];
+    console.log(storage);
+    for (i = 0; i < storage.length; i++) {
+        var cityList = $("<ul>").text(storage[i]);
+        $(".searchHistory").append(cityList);
+
+    }
+});
